aa5874747618e0c0de0f9bb9361958141e9619d6
dilbert250 <pickertjoe@gmail.com>
2019-08-25 08:07:55 -0500

    Finalized temperature plot

--- TEMP SCRIPT.R
+++ TEMP SCRIPT.R
@@ -19,22 +19,20 @@ colnames(MeanStudy) = c("Month", "Mean", "SE")
 
 TempG <- ggplot() + 
   
-  labs(y="Mean Daily temperature (C)", x="Month")+
-  geom_line(data=MeanT, aes(x=factor(Month), y=MeanT, color='Climatic Mean'), size=1.3, group=1)+
-  geom_ribbon(data=MeanT, aes(x=factor(Month), ymin=MeanT-SeT, ymax=MeanT+SeT, fill='Climatic Standard Error'), alpha=0.3, group=1)+
-  geom_point(data=MeanStudy, aes(x=factor(Month), y=Mean, color='2018 Mean'), size=3)+
-  geom_errorbar(data=MeanStudy, aes(x=factor(Month), ymin=Mean-SE, ymax=Mean+SE, color='2018 Standard Error'))+
-  scale_color_manual(values = c("Climatic Mean" = "dodgerblue4", 
-                                "2018 Mean" = "red", "2018 Standard Error" = "red"))+
-  scale_fill_manual(values = c("Climatic Standard Error" = "cyan"))+
+  labs(y="Mean Daily temperature (C)", x="Month", color="Time Period")+
+  theme_bw()+
+  geom_line(data=MeanT, aes(x=factor(Month), y=MeanT, color='1982-2017'), size=1.3, group=1)+
+  geom_ribbon(data=MeanT, aes(x=factor(Month), ymin=MeanT-SeT, ymax=MeanT+SeT, fill='Climatic Standard Error'), 
+              alpha=0.3, group=1, show.legend = FALSE)+
+  geom_pointrange(data=MeanStudy, aes(x=factor(Month), y=Mean, ymin=Mean-SE, ymax=Mean+SE, color='2018'), size=1)+
+  guides(errorbar=FALSE)+
+  scale_color_manual(values = c("1982-2017" = "dodgerblue4", 
+                                "2018" = "red"))+
+  scale_fill_manual(values = c("Climatic Standard Error" = "cyan", "2018 Standard Error" = "red"))+
   scale_x_discrete(breaks=c('1','2','3','4','5','6','7','8','9','10','11','12'),
                    labels=c("Jan","Feb","Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"))+
-  #scale_color_discrete()+
-  theme(plot.title = element_text(hjust=0.5))+
-  theme(legend.position ="bottom")+
-  ggtitle("Mean Temperatures for Study Period vs Climatic Average")+
-  theme_bw()
-
+  theme(plot.title = element_text(hjust=0.5), legend.position = 'bottom')+
+  ggtitle("Climatic Mean Temperatures vs 2018 Mean Temperatures")
 print(TempG)
 
 
