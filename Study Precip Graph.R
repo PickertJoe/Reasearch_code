035097e8e1e0a15f52cbf42ab75a11b51b2ec790
dilbert250 <pickertjoe@gmail.com>
2019-10-14 12:21:12 -0500

    Expanding study precip dataset to include water year 2017

--- Study Precip Graph.R
+++ Study Precip Graph.R
@@ -1,7 +1,7 @@
 library(ggplot2)
-setwd("~/Desktop/R_Scripts/Data/")
+setwd("~/R_Scripts/Data/")
 SPrecip <- read.csv("Precipagg.csv")
-SPrecip$Month <- factor(SPrecip$Month, levels=c("Oct","Nov","Dec","Jan","Feb","Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep"))
+SPrecip$Month <- factor(SPrecip$Month, levels=c("Oct '16", "Nov '16","Dec '16","Jan '17","Feb '17","Mar '17", "Apr '17", "May '17", "Jun '17", "Jul '17", "Aug '17", "Sep '17","Oct '17", "Nov '17","Dec '17","Jan '18","Feb '18","Mar '18", "Apr '18", "May '18", "Jun '18", "Jul '18", "Aug '18", "Sep '18"))
 
 Lower <- SPrecip$MPrecip - SPrecip$SE
 Upper <- SPrecip$MPrecip + SPrecip$SE
@@ -12,14 +12,15 @@ PrecipG <- ggplot(data=SPrecip, aes(x=SPrecip$Month, y=SPrecip$MPrecip, group=1)
   geom_ribbon(aes(x=SPrecip$Month, ymin=Lower, ymax=Upper),fill="forestgreen", alpha=0.9) +
   geom_line(aes( y =SPrecip$MPrecip), color="black")+
   geom_point(stat="identity", aes(y=SPrecip$APrecip), color="blue", size=3)+
-  ggtitle("Study Precip vs Climatic Average")+
+  ggtitle("Mean vs Observed Precipitation (WYs 2017 & 2018)")+
+  theme(plot.title = element_text(hjust=0.5))+
   scale_fill_manual(values=c("forest green", "navy"),
                     labels=c("Mean Precipitation", "Observed Precipitation"))+
   theme_bw()
 
 PrecipG
 
-setwd("~/Desktop/R_Scripts/Figures")
+setwd("~/R_Scripts/Figures")
 pdf("Study_Precip.pdf")
 print(PrecipG)
 dev.off()
